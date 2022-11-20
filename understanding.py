2b2092cb5f5f67d471c9373c3935627c5b00a981
freire-guido <14301656+freire-guido@users.noreply.github.com>
2022-11-20 19:28:15 -0300

    esc. 1

--- understanding.py
+++ understanding.py
@@ -3,11 +3,12 @@ from manim import *
 class Escena(Scene):
     def construct(self):
         axes = Axes(
-            x_range = [-10, 10, 1],
-            y_range = [-5, 10, 1]
+            x_range = [-5, 5, 1],
+            y_range = [-2, 5, 1],
+            tips = False
         )
-        title = MathTex(r"f_n(x) = \frac{x^2 + nx}{n}")
-        f_n = VGroup(axes.plot(lambda x: x ** 2 + x))
+        title = MathTex(r"f_n(x) = \frac{x^2 + nx}{n}", color = BLUE_A)
+        f_n = VGroup(axes.plot(lambda x: x ** 2 + x, color = BLUE_A))
         
         self.play(Write(title))
         self.wait()
@@ -17,13 +18,23 @@ class Escena(Scene):
             Create(f_n[0])
         )
         self.wait()
-        for n in range(1, 31):
-            f_n += axes.plot(lambda x: (x ** 2 + n*x) / n)
+        for n in range(1, 30, 5):
+            f_n += axes.plot(lambda x: (x ** 2 + n*x) / n, color = BLUE_A)
+        f_n += axes.plot(lambda x: x, color = BLUE)
         f_n_label = axes.get_graph_label(f_n[0], "n=1")
-        plot = VGroup(axes, f_n, f_n_label, title)
-        for n in range(2, 5):
+        plot = VGroup(axes, f_n)
+        for n in range(1, 6):
             self.play(
                 FadeTransform(f_n[n-1], f_n[n]),
                 Transform(f_n_label, axes.get_graph_label(f_n[0], f"n = {n}"))
             )
         self.wait()
+        self.play(
+            FadeTransform(f_n[5], f_n[len(f_n) - 1]),
+            Transform(f_n_label, axes.get_graph_label(f_n[0], MathTex("n = \infty"), color = BLUE))
+        )
+        self.wait(5)
+
+        self.clear()
+        converge = Text("¿Converge?")
+        self.play(Write(converge))
